a7785e56b7fe0e05e465882807414687c6a189f1
PR0TEX <protex19@protonmail.com>
2022-05-29 14:27:17 +0200

    Garbage imports and no used excption deleted

--- src/main/java/com/pawcie/authorization/jwt/JwtTokenRefresh.java
+++ src/main/java/com/pawcie/authorization/jwt/JwtTokenRefresh.java
@@ -1,48 +1,27 @@
 package com.pawcie.authorization.jwt;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.pawcie.authorization.entities.User;
-import com.pawcie.authorization.services.UserService;
 import com.pawcie.authorization.users.ApplicationUser;
 import com.pawcie.authorization.users.ApplicationUserDetailsService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
-import org.apache.catalina.Role;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.Date;
-import java.time.LocalDate;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @RestController
@@ -57,7 +36,7 @@ public class JwtTokenRefresh {
     private ApplicationUserDetailsService applicationUserDetailsService;
 
     @GetMapping("/refresh")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws JSONException, IOException {
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
             try {
                 String refreshToken = new ObjectMapper()
                         .readValue(request.getInputStream(), String.class);;
